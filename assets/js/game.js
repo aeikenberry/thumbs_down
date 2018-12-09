98249b184adcc82e281fdd8750f53a71ed78e6f7
Aaron Eikenberry <aeikenberry@gmail.com>
2018-12-09 11:30:06 -0600

    spacing

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,51 +1,50 @@
 import {Socket} from "phoenix"
 
 export default class ThumbsDown {
-    constructor(roomId, name) {
-        this.roomId = roomId
-        this.username = name
-        this.socket = new Socket("/socket", {})
-        this.socket.connect()
-        this.channel = this.socket.channel(`room:${roomId}`, {name: name})
-        this.users = []
-    }
+  constructor(roomId, name) {
+    this.roomId = roomId
+    this.username = name
+    this.socket = new Socket("/socket", {})
+    this.socket.connect()
+    this.channel = this.socket.channel(`room:${roomId}`, {name: name})
+  }
 
-    setup() {
-      this.channel.join()
-        .receive('ok', this.handleEnter.bind(this))
-        .receive('error', this.handleError.bind(this))
-      this.channel.on('presence_state', this.handlePresenseState.bind(this))
-      this.channel.on('presence_diff', this.handlePresenseChange.bind(this))
-      this.channel.on('game_start', this.handleGameStart.bind(this))
-      this.channel.on('game_over', this.handleGameOver.bind(this))
-      this.channel.on('user_change', this.handleUserChange.bind(this))
-    }
+  setup() {
+    this.channel.join()
+      .receive('ok', this.handleEnter.bind(this))
+      .receive('error', this.handleError.bind(this))
+    this.channel.on('presence_state', this.handlePresenseState.bind(this))
+    this.channel.on('presence_diff', this.handlePresenseChange.bind(this))
+    this.channel.on('game_start', this.handleGameStart.bind(this))
+    this.channel.on('game_over', this.handleGameOver.bind(this))
+    this.channel.on('user_change', this.handleUserChange.bind(this))
+  }
 
-    handleEnter(payload) {
-        console.log('Joined Room', payload)
-    }
+  handleEnter() {
+    console.log('Joined Room')
+  }
 
-    handlePresenseChange(payload) {
-        console.log('Received presense change', payload)
-    }
+  handlePresenseChange(payload) {
+    console.log('Received presense change', payload)
+  }
 
-    handlePresenseState(payload) {
-        console.log('Presense State', payload)
-    }
+  handlePresenseState(payload) {
+    console.log('Presense State', payload)
+  }
 
-    handleError(err) {
-        console.log(err)
-    }
+  handleError(err) {
+    console.log(err)
+  }
 
-    handleGameStart() {
+  handleGameStart() {
 
-    }
+  }
 
-    handleGameOver() {
+  handleGameOver() {
 
-    }
+  }
 
-    handleUserChange() {
+  handleUserChange() {
 
-    }
+  }
 }
