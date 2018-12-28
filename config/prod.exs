6394701672f123a18b4d83b930b1d6c55493e92b
Aaron Eikenberry <aeikenberry@gmail.com>
2018-12-27 23:32:05 -0600

    host_url

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :thumbs_down, ThumbsDownWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "thumbs-down-dev.herokuapp.com", port: 443],
+  url: [scheme: "https", host: System.get_env("HOST_URL"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
